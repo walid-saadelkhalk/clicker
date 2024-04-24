d47a22307973a41054cffef30204a25fd7861342
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-24 10:38:49 +0200

    [UPDATE] in index.html, each helper name add. in the style.css, each name is put side to each button

--- levelUp.js
+++ levelUp.js
@@ -1,36 +1,3 @@
-// var levelElement = document.getElementById('currentLevel');
-// var level = 1;
-
-// function updateLevel() {
-//     var totalClicks = transactionList.length;
-//     console.log("Nombre total de clics : ", totalClicks);
-
-//     if (totalClicks >= 100) {
-//         level = 5;
-//         count += BigInt(4);
-//     } else if (totalClicks >= 80) {
-//         level = 4;
-//         count += BigInt(3);
-//     } else if (totalClicks >= 40) {
-//         level = 3;
-//         count += BigInt(2);
-//     } else if (totalClicks >= 20) {
-//         level = 2;
-//         count += BigInt(1);
-//     } else {
-//         level = 1;
-//         count += BigInt(0);
-//     } 
-
-//     levelElement.textContent = level.toString();
-// }
-
-// updateLevel();
-
-// button.addEventListener('click', function() {
-//     updateLevel();
-// });
-
 var levelElement = document.getElementById('currentLevel');
 var level = 1;
 
@@ -64,27 +31,15 @@ function updateLevel() {
     levelElement.textContent = level.toString();
 }
 
-// function showClicksEarned(clicks) {
-//     var clicksEarnedContainer = document.getElementById('clicksEarnedContainer');
-//     clicksEarnedContainer.textContent = "+" + clicks;
-//     setTimeout(function() {
-//         clicksEarnedContainer.textContent = ""; 
-//     }, 100); 
-// }
 function showClicksEarned(clicks) {
     var clicksEarnedContainer = document.getElementById('clicksEarnedContainer');
-    var currentClicks = parseInt(clicksEarnedContainer.textContent.replace("+", ""));
-    var totalClicks = currentClicks + clicks;
-    
-    clicksEarnedContainer.textContent = "+" + totalClicks;
-    clicksEarnedContainer.classList.add('clickAnimation'); // Ajoute la classe d'animation
-    
+    clicksEarnedContainer.textContent = "+" + clicks;
+    clicksEarnedContainer.classList.add('clickAnimation');
     setTimeout(function() {
-        clicksEarnedContainer.classList.remove('clickAnimation'); // Supprime la classe d'animation après 2 secondes
-    }, 2000); // 2 secondes
+        clicksEarnedContainer.classList.remove('clickAnimation'); 
+        clicksEarnedContainer.textContent = "";
+    }, 1000);
 }
-
-
 
 updateLevel();
 
