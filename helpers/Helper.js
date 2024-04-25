8976668634e50291af2983271573dbd769f14af8
Dhjali <barbara.vanni@laplateforme.io>
2024-04-25 21:59:06 +0200

    [MODIFY] change the buy function

--- helpers/Helper.js
+++ helpers/Helper.js
@@ -2,22 +2,37 @@ class Helper {
     constructor(price, baseCapacity) {
         this.price = BigInt(price);
         this.baseCapacity = BigInt(baseCapacity);
-        this.capacity = this.baseCapacity;
+        this.capacity = BigInt(baseCapacity);
         this.active = false;
         this.interval = null;
     }
 
-    // buy helper for click
     buy() {
         if (!this.active && count >= this.price) {
-            count -= this.price;
+            count -= this.price; 
+            this.price *= 2n;
             this.active = true;
-            this.price *= 2n; 
             this.interval = setInterval(() => {
                 this.updateCounter();
             }, 1000);
         }
     }
+
+
+
+
+    // buy helper for click
+    // buy() {
+    //     if (!this.active && count >= this.price) {
+    //         count -= this.price;
+    //         this.price *= 2n; 
+    //         this.active = true;
+    //         this.interval = setInterval(() => {
+    //             this.updateCounter();
+    //         }, 1000);
+    //     }
+        
+    // }
 
     upgrade() {
         // Empty now but can be overridden in subclasses
