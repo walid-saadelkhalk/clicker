77022236a811da131407f95c08d84c343c13f491
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-26 10:31:47 +0200

    [SAVE] commit the branch to have the possibility pulling

--- clicker.js
+++ clicker.js
@@ -1,3 +1,56 @@
+// var button = document.getElementById('clickerButton');
+// var counter = document.getElementById('clickerCounter');
+// var cpsCounter = document.getElementById('cpsCounter');
+
+// var count = BigInt(0);
+// var transactionList = [];
+// var clicksPerSecond = 0;
+
+// var helper2Active = false;
+
+
+// function updateCounter() {
+//     count += 1;
+//     counter.textContent = count;
+// }
+
+// // let count = BigInt(0n);
+// // let percentageIncrease = 5n; // 10%
+
+// // function updateCounter() {
+// //     let increase = count < 100n ? 1n : count * percentageIncrease / 100n;
+// //     count += increase;
+// //     counter.textContent = count.toString();
+// // }
+
+// button.addEventListener('click', function() {
+//     updateCounter();
+//     document.body.style.backgroundImage = "url('assets/images/ps1_on.png')";
+//     document.body.style.backgroundSize = "100% 100%";
+//     document.body.style.backgroundRepeat = "no-repeat";
+//     document.body.style.backgroundPosition = "center";
+//     setTimeout(function() {
+//         document.body.style.backgroundImage = "";
+//     }, 500);
+//     transactionList.push({
+//         clickCounter: count,
+//         priceHelper1: BigInt(priceHelper1.textContent),
+//         priceHelper2: BigInt(priceHelper2.textContent)
+//     });
+// });
+
+
+
+// // Click per second
+// setInterval(function() {
+//     cpsCounter.textContent = "Cps: " + clicksPerSecond;
+//     // clicksPerSecond = 0; // Réinitialiser le compteur de clics par seconde
+// }, 1000);
+
+
+// // button.addEventListener('click', function() {
+// //     clicksPerSecond++;
+// // });
 var button = document.getElementById('clickerButton');
 var counter = document.getElementById('clickerCounter');
 var cpsCounter = document.getElementById('cpsCounter');
@@ -10,18 +63,9 @@ var helper2Active = false;
 
 
 function updateCounter() {
-    count += 1;
-    counter.textContent = count;
+    count += BigInt(1);
+    counter.textContent = count.toString();
 }
-
-// let count = BigInt(0n);
-// let percentageIncrease = 5n; // 10%
-
-// function updateCounter() {
-//     let increase = count < 100n ? 1n : count * percentageIncrease / 100n;
-//     count += increase;
-//     counter.textContent = count.toString();
-// }
 
 button.addEventListener('click', function() {
     updateCounter();
@@ -38,7 +82,6 @@ button.addEventListener('click', function() {
         priceHelper2: BigInt(priceHelper2.textContent)
     });
 });
-
 
 
 // Click per second
