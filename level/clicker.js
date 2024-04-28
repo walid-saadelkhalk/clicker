b2c263a7e2bcf3e6e506d31d1f39a8b84c753f5e
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-28 20:39:39 +0200

    [CREATE] new file goldenButton with the function

--- level/clicker.js
+++ level/clicker.js
@@ -45,29 +45,5 @@ setInterval(function() {
 // });
 
 
-function handleGoldButtonClick() {
-    count += BigInt(100);
-    counter.textContent = count.toString();
-    localStorage.setItem('gamer', count.toString());
-}
 
-goldButton.addEventListener('click', handleGoldButtonClick);
-
-function showGoldButton() {
-    goldButton.style.visibility = 'visible';
-}
-
-function hideGoldButton() {
-    goldButton.style.visibility = 'hidden';
-}
-
-goldButton.style.visibility = 'hidden';
-
-setTimeout(showGoldButton, 5 * 60 * 1000);
-
-
-setInterval(function() {
-    showGoldButton();
-    setTimeout(hideGoldButton, 3000); 
-}, 5 * 60 * 1000);
 
