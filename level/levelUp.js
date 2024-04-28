a58ff13a28220291381d802f41d5dc71a923eebc
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-28 20:29:59 +0200

    [CREATE] we have now an access at diferent worlds in function of the level

--- level/levelUp.js
+++ level/levelUp.js
@@ -64,51 +64,53 @@ function updateLevel(totalClicks) {
         level = 10;
         count += BigInt(9);
         showClicksEarned(10);
+        document.getElementById('worlds-banner').classList.add('level-10');
     } else if (totalClicks >= 130) {
         level = 9;
         count += BigInt(8);
         showClicksEarned(9);
+        document.getElementById('worlds-banner').classList.add('level-9');
     } else if (totalClicks >= 120) {
         level = 8;
         count += BigInt(7);
         showClicksEarned(8);
+        document.getElementById('worlds-banner').classList.add('level-8');
     } else if (totalClicks >= 110) {
         level = 7;
         count += BigInt(6);
         showClicksEarned(7);
+        document.getElementById('worlds-banner').classList.add('level-7');
     } else if (totalClicks >= 100) {
         level = 6;
         count += BigInt(5);
         showClicksEarned(6);
+        document.getElementById('worlds-banner').classList.add('level-6');
     } else if (totalClicks >= 80) {
         level = 5;
         count += BigInt(4);
         showClicksEarned(5);
+        document.getElementById('worlds-banner').classList.add('level-5');
     } else if (totalClicks >= 60) {
         level = 4;
         count += BigInt(3);
         showClicksEarned(4);
+        document.getElementById('worlds-banner').classList.add('level-4');
     } else if (totalClicks >= 40) {
         level = 3;
         count += BigInt(2);
         showClicksEarned(3);
+        document.getElementById('worlds-banner').classList.add('level-3');
     } else if (totalClicks >= 20) {
         level = 2;
         count += BigInt(1);
         showClicksEarned(2);
+        document.getElementById('worlds-banner').classList.add('level-2');
     } else {
         level = 1;
         count += BigInt(0);
         showClicksEarned(1);
     }
-        // let worldIcons = document.querySelectorAll('.world-icon');
-    // worldIcons.forEach(icon => {
-    //     icon.style.filter = ''; // Réinitialise d'abord tous les filtres
-    //     if (level >= 2 && level <= 6) {
-    //         // Selon le niveau, ajustez le filtre pour ne pas griser le monde
-    //         let grayscaleValue = (6 - level) * 20; // Calcule la valeur de gris en fonction du niveau
-    //         icon.style.filter = `grayscale(${grayscaleValue}%)`;
-    // }
+
     levelElement.textContent = level.toString();
     localStorage.setItem('level', level.toString());
 }
